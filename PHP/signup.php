7b5132534e7d397c3eec40cc8cef84d5bddfa322
Hasnain-iut <94733956+Hasnain-iut@users.noreply.github.com>
2022-09-10 23:14:08 +0600

    Signup done
    [IMPORTANT]
    Make sure to change the column names Previous Medical History to Previous_Medical_History and Vaccination status to Vaccination_status
    
    Add another column called Email under ID column and set Email to unique.
    
    Make sure to copy your own code to your system before pulling my code.

---
+++ PHP/signup.php
@@ -0,0 +1,92 @@
+<?php
+
+if(isset($_POST["signup-submit"])){
+    require '../SQL/dbConnect.php';
+
+    $med_history = $vaccination = "";
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $conf_password = $_POST['confirm_password'];
+    $date_of_birth = $_POST['birth_date'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+    $place_of_birth = $_POST['birth_place'];
+    $med_history = $_POST['medical_history'];
+    $vaccination = $_POST['vaccination_status'];
+
+
+    if(empty($name) ||empty($email) ||empty($password) ||empty($conf_password) ||empty($date_of_birth) ||
+    empty($gender) ||empty($age) ||empty($place_of_birth)){
+        header("Location: ../signup.html?error=emptyfields&name=".$name."&mail=".$email);
+        exit();
+    }
+
+    elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        header("Location: ../signup.html?error=invalidmail&name=".$name);
+        exit();
+    }
+    
+    elseif($password !== $conf_password){
+        header("Location: ../signup.html?error=passwordcheck&name=".$name. "&mail=".$email);
+        exit();
+    }
+
+    else{
+        $sql = "SELECT Email FROM patient WHERE Email=?";
+        $statement = mysqli_stmt_init($conn);
+        
+        if(!mysqli_stmt_prepare($statement, $sql)){
+            header("Location: ../signup.html?error=sqlerror1");
+            exit();
+        }
+        else{
+            mysqli_stmt_bind_param($statement, "s", $email);
+            mysqli_stmt_execute($statement);
+            mysqli_stmt_store_result($statement);
+            $result_check = mysqli_stmt_num_rows($statement);
+
+            if($result_check>0){
+                header("Location: ../signup.html?error=emailtaken&name=".$email);
+            }
+            
+            else{
+
+                $sql = "INSERT INTO patient (Name, Email, Password, Gender, Birth_date,
+                Age, Birth_place, Previous_Medical_History, Vaccination_status) VALUES (?,?,?,?,?,?,?,?,?)";
+                $statement = mysqli_stmt_init($conn);
+        
+                if(!mysqli_stmt_prepare($statement, $sql)){
+                    $err = mysqli_stmt_error($statement);
+                    header("Location: ../signup.html?error=sqlerror".$err);
+                    exit();
+                }
+                else{
+
+                    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+                    mysqli_stmt_bind_param($statement, "sssssssss", $name, $email, $hashed_password,
+                                        $gender, $date_of_birth, $age, $place_of_birth, $med_history, $vaccination);
+                    mysqli_stmt_execute($statement);
+                    header("Location: ../signup.html?signup=success");
+                    exit();
+                }
+            }
+        }
+    }
+
+    mysqli_stmt_close($statement);
+    mysqli_close($conn);
+
+}
+else
+{
+    header("Location: ../signup.php");
+    exit();
+
+}
+
+
+
+?>
