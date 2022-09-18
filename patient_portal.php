2d9b76bf6074e74a5f9597aafa3f23f9d5a8a1ed
Hasnain-iut <hasnainkabir@iut-dhaka.edu>
2022-09-18 23:12:33 +0600

    department added

--- patient_portal.php
+++ patient_portal.php
@@ -50,15 +50,16 @@
 
           <li class="dropdown"><span>Departments</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="#">Show All Departments</a></li>
-              <li><a href="#">Medicine and Nephrology</a></li>
+              <li><a class="nav-link scrollto" href="#departments">Show All Departments</a></li>
+              <li><a href="#">Medicine</a></li>
               <li><a href="#">Psychiatry</a></li>
               <li><a href="#">Cardiology</a></li>
               <li><a href="#">General Surgeon</a></li>
-              <li><a href="#">Neurosurgery</a></li>
+              <li><a href="#">Neurology</a></li>
               <li><a href="#">E.N.T</a></li>
-              <li><a href="#">Orthopedic Surgery</a></li>
-              <li><a href="#">Gynaecology and Obstetrics</a></li>
+              <li><a href="#">Orthopedics</a></li>
+              <li><a href="#">Gynaecology
+              </a></li>
               <li><a href="#">Skin and VD</a></li>
               
             </ul>
@@ -154,14 +155,16 @@
               <label for="department" name="dept-label">Choose your department</label>
               <select name="department" id="department" class="form-select">
                 <option value="">Select Department</option>
-                <option value="#">Medicine and Nephrology</option>
+                <option value="#">Medicine</option>
+                <option value="#">Eye Care</option>
                 <option value="#">Psychiatry</option>
                 <option value="#">Cardiology</option>
                 <option value="#">General Surgeon</option>
-                <option value="#">Neurosurgery</option>
+                <option value="#">Neurology</option>
                 <option value="#">E.N.T</option>
-                <option value="#">Orthopedic Surgery</option>
-                <option value="#">Gynaecology and Obstetrics</option>
+                <option value="#">Orthopedics
+                </option>
+                <option value="#">Gynaecology</option>
                 <option value="#">Skin and VD</option>
               </select>
               <div class="validate"></div>
@@ -185,12 +188,219 @@
         </div>
     </section>
 
+    <section class="departments" id="departments">
+      <div class="container">
+        <div class="section-title">
+          <h2>Departments</h2>
+          <p>Our doctors are divided into a range of departments through which you can easily select the type of doctor you want 
+            to get an appointment of.
+          </p>
+        </div>
 
-    <!-- Department-->
-    <section class="department">
 
+        <div class="row gy-4">
+          <div class="col-lg-3">
+            <ul class="nav nav-tabs flex-column">
+              <li class="nav-item">
+                <a class="nav-link active show" data-bs-toggle="tab" href="#tab-1">Medicine</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="tab" href="#tab-2">Eye Care</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="tab" href="#tab-3">Psychiatry</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="tab" href="#tab-4">Cardiology</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="tab" href="#tab-5">General Surgeon</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="tab" href="#tab-6">Neurology</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="tab" href="#tab-7">E.N.T</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="tab" href="#tab-8">Orthopedics</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="tab" href="#tab-9">Gynaecology</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="tab" href="#tab-10">Skin and VD</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-9">
+            <div class="tab-content">
+              <div class="tab-pane active show" id="tab-1">
+                <div class="row gy-4">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Medicine</h3>
+                    <p class="fst-italic">Doctor of internal medicine</p>
+                    <p>Physians practicing internal medicine. A broad-based medical field in which physicians rely 
+                      on their knowledge of major organs to diagnose and treat patients. 
+                      Internists treat a variety of afflictions, from colds and heart problems to infectious diseases.
+                       Internists often serve as a patient's primary doctor, coordinating all that person's health care.</p>
+                  </div>
+                  <div class="col-lg-4 text-center order-1 order-lg-2">
+                    <img src="assets/img/departments-1.jpg" alt="" class="img-fluid">
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane" id="tab-2">
+                <div class="row gy-4">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Eye Care</h3>
+                    <p class="fst-italic">Specializing in eye vision and care.</p>
+                    <p>A physician who specializes in the diagnosis and treatment of diseases of the eye. Ophthalmologists may prescribe and fit 
+                      eyeglasses and contact lenses and also treat eye diseases with drugs and surgery. He/She will 
+                      prescribe and fit glasses and contact lenses, as well as prescribe therapeutic exercises</p>
+                  </div>
+                  
+                </div>
+              </div>
+              <div class="tab-pane" id="tab-3">
+                <div class="row gy-4">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Psychiatry</h3>
+                    <p class="fst-italic">Physians speciallizing on pyschology and human behaviour.</p>
+                    <p>A psychiatrist is a medical doctor who specializes in mental health, including substance use disorders. Psychiatrists are qualified to assess both the mental and physical aspects of psychological problems.
+People seek psychiatric help for many reasons. The problems can be sudden, such as a panic attack, frightening hallucinations, thoughts of suicide, or hearing "voices." Or they may be more long-term, such as feelings of sadness, hopelessness, or 
+anxiousness that never seem to lift or problems functioning, 
+causing everyday life to feel distorted or out of control.</p>
+                  </div>
+                  
+                </div>
+              </div>
+              <div class="tab-pane" id="tab-4">
+                <div class="row gy-4">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Cardiology</h3>
+                    <p class="fst-italic">An expert in heart and blood vessel diseases</p>
+                    <p>A cardiologist is a healthcare provider who can treat chest pain, high blood pressure and heart failure, as well as problems with your heart valves, blood vessels and other heart and vascular issues. They can order tests like electrocardiograms, echocardiograms and CTs (computed tomography) to find out what’s wrong. With their diagnosis, they can order medicine, help you start healthier exercise and eating habits or do cardiac catheterization.</p>
+                  </div>
+                  
+                </div>
+              </div>
+
+              <div class="tab-pane" id="tab-5">
+                <div class="row gy-4">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>General Surgeon</h3>
+                    <p class="fst-italic">Specializing in surgical procedures</p>
+                    <p>A general surgeon has specialized knowledge of the entire surgical process, from the initial evaluation through preparation, procedure, and post-operative management.
+                    General surgeons have a broad knowledge of many different diseases and conditions. They will recommend whether you need surgery and what kind of surgery would be appropriate.
+                    </p>
+                  </div>
+                  
+                </div>
+              </div>
+
+
+              <div class="tab-pane" id="tab-6">
+                <div class="row gy-4">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Neurology</h3>
+                    <p class="fst-italic">Manage and treat neurological conditions, or problems with the nervous system</p>
+                    <p>A neurologist is a medical doctor who specializes in treating diseases of the nervous system. The nervous system is made of two parts: the central and peripheral nervous system. It includes the brain and spinal cord.
+Illnesses, disorders, and injuries that involve the nervous system often require a neurologist’s management and treatment.
+                    </p>
+                  </div>
+                  
+                </div>
+              </div>
+
+
+              <div class="tab-pane" id="tab-7">
+                <div class="row gy-4">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>E.N.T</h3>
+                    <p class="fst-italic">A physician specializing in medical conditions of the ear, nose, throat, and neck </p>
+                    <p>Hearing and balance, swallowing and speech, breathing and sleep issues, allergies and sinuses, head and neck cancer, 
+                      skin disorders, even facial plastic surgery are just some of the conditions that “ENT” (ear, nose, and throat) specialists treat.
+                      ENT specialists are not only medical doctors who can treat your sinus headache, your child’s swimmer’s ear, or your dad’s sleep apnea. They are also surgeons who can perform extremely 
+                      delicate operations to restore hearing of the middle ear, open blocked airways, remove head, neck, and throat cancers, and rebuild these essential structures.
+                    </p>
+                  </div>
+                  
+                </div>
+              </div>
+
+
+              <div class="tab-pane" id="tab-8">
+                <div class="row gy-4">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Orthopedics</h3>
+                    <p class="fst-italic">Specializing in diseases affecting your musculoskeletal system.</p>
+                    <p>An orthopedist (also spelled orthopaedist) is a medical specialty focusing on injuries and diseases affecting your
+                       musculoskeletal system (bones, muscles, joints and soft tissues). 
+                      Although this type of doctor is a surgeon, they often help people get relief with nonsurgical therapies.
+                      Orthopedics focuses on issues due to injury, congenital defects and wear and tear (degenerative disease).
+                    </p>
+                  </div>
+                  
+                </div>
+              </div>
+
+
+              <div class="tab-pane" id="tab-9">
+                <div class="row gy-4">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Gynaecology</h3>
+                    <p class="fst-italic">Specializing in female reproductive health</p>
+                    <p>Gynecologists give reproductive health services that include pelvic exams, 
+                      Pap tests, cancer screenings, and testing and treatment for vaginal infections.
+                      They diagnose and treat reproductive system disorders such as endometriosis, infertility, ovarian cysts,
+                      and pelvic pain. They may also care for people with ovarian, cervical, and other reproductive cancers.
+                      </p>
+                  </div>
+                  
+                </div>
+              </div>
+
+
+              <div class="tab-pane" id="tab-10">
+                <div class="row gy-4">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Skin and VD</h3>
+                    <p class="fst-italic">Specialist in dermatology and venereology</p>
+                    <p>The dermatologist is an expert in skin diseases and sexually transmitted diseases. 
+                      Eczema, acne, psoriasis, vitiligo and allergies are the most common indications for dermatological treatment. 
+                      The most frequent types of therapy for skin diseases include operative removals of benign and malignant skin changes - malignant melanoma (skin cancer).
+Dermatologists possess expertise in diagnostic procedures as well as disease-related pre- and post-diagnostic treatments.
+                    </p>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
 
     </main>
+
+    <!--Footer-->
+    <footer id="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col footer-address">
+              <h3>Med.io</h3>
+              <p>Address: </p>
+              <p>Contact: +8801747116015, +8801830638230, +8801764257445
+              </p>
+          </div>
+          <div class="col footer-icon-links">
+              <h5>Icons by</h5>
+              <a href="https://freeicons.io">Freeicons</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+
     <script src="assets/js/patient_portal_script.js"></script>
     </body>
 </html>
