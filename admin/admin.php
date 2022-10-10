8b884bf5c422d5da737ac6e3fcf5d948e1ac0443
Mukit Mahdin <88025578+mahdin70@users.noreply.github.com>
2022-10-10 21:54:45 +0600

    Added Admin Panel + ADD + Remove Admin + Fetching from Database

---
+++ admin/admin.php
@@ -0,0 +1,151 @@
+<?php
+session_start();
+error_reporting(0);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.1.slim.js" integrity="sha256-tXm+sa1uzsbFnbXt8GJqsgi2Tw+m4BLGDof6eUPjbtk=" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    </head>
+    <body>
+      
+    <body style="background-color:white;">
+        <?php
+        include("../SQL/dbConnect.php")
+        ?>
+        <nav class="navbar navbar-expand-lg navbar-info bg-dark">
+            <img src="../assets/images/admin_logo.png" height="32px" width="32px" style="padding:5px;" />
+            <h5 class="text-white">Admin Panel</h5>
+            <div class="mr-auto"></div>
+
+            <ul class="navbar-nav">
+                    <li class="nav-item"><a href="#" class="nav-link text-white">Admin</a></li>
+                    <li class="nav-item"><a href="/Med.io/admin_login.php" class="nav-link text-white">Logout</a> </li>
+            </ul>
+        </nav>
+
+        <div class="container-fluid">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-2" style="margin-left:-30px";>
+
+            <!--Side Navigation-->
+                         <?php
+                           include("./sidenav.php");
+                           include("../SQL/dbConnect.php")
+
+                         ?>
+                    </div>
+                    <div class="col-md-10">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h5 class="text-center">All Admin</h5>
+
+                                    <?php
+                            
+                                     $ad = $_SESSION['admin'];
+                                     $query = "SELECT * FROM admin WHERE User !='$ad'";
+                                     $res = mysqli_query($conn,$query);
+                                     $output =
+                                     '<table class="table table-bordered">
+                                     <th>Username</th>
+                                     <th style="width:10%;">Action</th>
+                                     <tr>
+                                     ';
+
+                                     if(mysqli_num_rows($res)< 1){
+                                        $output ="<tr><td colspan='2' class='text-center'>No New Admin</td></tr>";
+                                     }
+
+                                     while($row = mysqli_fetch_array($res)){
+                                        $user = $row['User'];
+                                        $output .='
+                                        <tr>
+                                            <td>'.$user.'</td>
+                                            <td>
+                                                <a href="admin?user='.$user.'"><button id='.$user.' class="btn btn-danger remove">Remove</button></a>
+                                            </td>';
+                                     }
+                                     $output .="
+                                     </tr>
+                                     </table>";
+
+                                     echo $output;
+
+                                     if(isset($_GET['User'])){
+                                        $user = $_GET['User'];
+
+                                        $query = "DELETE FROM admin WHERE User='$user'";
+                                        mysqli_query($conn,$query);
+                                     }
+                                    ?>
+            
+
+                                </div>
+                                <div class="col-md-6">
+
+                                <?php
+
+                                if(isset($_POST['add'])){
+                                    $user = $_POST['user'];
+                                    $password = $_POST['password'];
+
+                                    $error = array();
+
+                                    if(empty($user)){
+                                        $error['u'] = "Enter Admin Username!";
+                                    }
+                                    else if(empty($password)){
+                                        $error['u'] = "Enter Admin Password!";
+                                    }
+
+                                    if(count($error)==0){
+                                        $q = "INSERT INTO admin(User,Password) VALUES('$user','$password')";
+                                        $result = mysqli_query($conn,$q);
+                                    }
+                                }
+
+                                if(isset($error['u'])){
+                                    $er = $error['u'];
+
+                                    $show = "<h5 class='text-center alert alert-danger'>$er</h5>";
+                                }
+                                else{
+                                    $show = "";
+                                }
+                                ?>
+                                    <h5 class="text-center">Add Admin</h5>
+                                    <form method="post" enctype="multipart/form-data">
+                                        <div class="form-group">
+                                            <label>Username</label>
+                                            <input type="text" name="user" class="form-control">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Password</label>
+                                            <input type="password" name="password" class="form-control">
+                                        </div>
+                                        <input type="submit" name="add" value="Add New Admin" class="btn btn-success">
+                                    </form>
+                                    
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- admin/index.php
+++ admin/index.php
@@ -5,30 +5,38 @@ session_start();
 <html>
     <head>
      
-    <title>Admin Login Page</title>
+    <title>Admin Dashboard</title>
 
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.6.1.slim.js" integrity="sha256-tXm+sa1uzsbFnbXt8GJqsgi2Tw+m4BLGDof6eUPjbtk=" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
 
     
     </head>
 
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-info bg-info">
+    <body style="background-color:darkslategray;">
+        <?php
+        include("../SQL/dbConnect.php")
+        ?>
+        <nav class="navbar navbar-expand-lg navbar-info bg-dark">
+            <img src="../assets/images/admin_logo.png" height="32px" width="32px" style="padding:5px;" />
             <h5 class="text-white">Admin Panel</h5>
             <div class="mr-auto"></div>
 
             <ul class="navbar-nav">
-
-                    <li class="nav-item"><a href="#" class="nav-link text-white">admin</a></li>
-                    <li class="nav-item"><a href="/Med.io/admin_login.php" class="nav-link text-white">Log Out</a> </li>
+                    <li class="nav-item"><a href="#" class="nav-link text-white">Admin</a></li>
+                    <li class="nav-item"><a href="/Med.io/admin_login.php" class="nav-link text-white">Logout</a> </li>
             </ul>
         </nav>
 
-        
         <div class="container-fluid">
             <div class="col-md-12">
                 <div class="row">
@@ -36,11 +44,116 @@ session_start();
 
             <!--Side Navigation-->
                          <?php
-                           include("sidenav.php")
+                           include("./sidenav.php")
                          ?>
+                    </div>
+                    <div class="col-md-10 ">
+
+
+            <!--Dashboard and Cardview-->
+
+                    <div class="col-md-12 my-5">
+                        <div class="row">
+
+                        <!--Total Admin-->
+
+                            <div class="col-md-3 bg-info mx-2" style="height:130px; border-radius:10px;">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        
+                                        <?php
+                                        $ad = mysqli_query($conn,"SELECT * FROM admin");
+                                        $num = mysqli_num_rows($ad);
+                                        ?>
+
+                                        <h5 class="my-2 text-white" style="font-size:30px;"><?php echo $num; ?></h5>
+                                        <h5 class="text-white">Total</h5>
+                                        <h5 class="text-white">Admin</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <a href="../admin/admin.php"><i class="fa fa-solid fa-user-shield fa-3x my-4" style="color:azure;"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+
+                            <!--Total Doctor-->
+
+                            <div class="col-md-3 bg-info mx-2" style="height:130px; border-radius:10px;">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <h5 class="my-2 text-white" style="font-size:30px;">0</h5>
+                                        <h5 class="text-white">Total</h5>
+                                        <h5 class="text-white">Doctor</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <a href="#"><i class="fa fa-solid fa-user-doctor fa-3x my-4" style="color:azure;"></i></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+
+                            <!--Total Patient-->
+                            <div class="col-md-3 bg-info mx-2" style="height:130px; border-radius:10px;">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <h5 class="my-2 text-white" style="font-size:30px;">0</h5>
+                                        <h5 class="text-white">Total</h5>
+                                        <h5 class="text-white">Patient</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <a href="#"><i class="fa fa-solid fa-hospital-user fa-3x my-4" style="color:azure;"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+
+                            <!--Total Report-->
+
+                            <div class="col-md-3 bg-danger mx-2 my-3" style="height:130px; border-radius:10px;">
+                            <div class="col-md-18">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <h5 class="my-2 text-white" style="font-size:30px;">0</h5>
+                                        <h5 class="text-white">Total</h5>
+                                        <h5 class="text-white">Report</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <a href="#"><i class="fa fa-solid fa-file-contract fa-3x my-4" style="color:azure;"></i></a>
+
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+
+
+                            <!--Total Job Request-->
+
+                            <div class="col-md-3 bg-warning mx-2 my-3" style="height:130px; border-radius:10px;">
+                            <div class="col-md-18">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <h5 class="my-2 text-black" style="font-size:30px;">0</h5>
+                                        <h5 class="text-black">Total</h5>
+                                        <h5 class="text-black">Request</h5>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <a href="#"><i class="fa fa-solid fa-people-group fa-3x my-4" style="color:azure;"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+                       
+                        </div>
+                      </div>
                     </div>
                 </div>
             </div>
         </div>
+
+
     </body>
 </html>
