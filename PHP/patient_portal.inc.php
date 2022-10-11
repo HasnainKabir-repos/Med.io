d4a9bfd93e00bc52278dab32779eaecc6c0dbf37
Hasnain Kabir <hasnainkabir@iut-dhaka.edu>
2022-10-11 21:19:02 +0600

    requests done
    Create table requests to make sure it runs. Table must have ReqID (Auto Increment) PatientID INT, date DATE, message TEXT, department TEXT, Assignement tinyInt, DoctorID INT.
    
    Commented Jquery statement because we do not need it.

--- PHP/patient_portal.inc.php
+++ PHP/patient_portal.inc.php
@@ -2,9 +2,43 @@
     session_start();
    
     if (isset($_POST["appointment-submit"])) {
+        require '../SQL/dbConnect.php';
+
+        $date = $_POST["appointment-date"];
+        $department = $_POST["department"];
+        $message = $_POST["message"];
+
+
         if(!(isset($_SESSION['patientloggedin']))){
             header("Location: ../Login.php?error=notloggedin");
             exit();
+        }
+
+        else if(empty($date) || empty($department)){
+            header("Location: ../patient_portal.php?error=emptyfields");
+            exit();
+        }else{
+
+            $patientID = $_SESSION['patientID'];
+            
+            $sql = "INSERT INTO requests (PatientID, date, message, department) VALUES (?,?,?,?)";
+            $statement = mysqli_stmt_init($conn);
+        
+            if(!mysqli_stmt_prepare($statement, $sql)){
+
+                $err = mysqli_stmt_error($statement);
+
+                header("Location: ../patient_portal.php?error=sqlerror".$err);
+                exit();
+            }else{
+
+                mysqli_stmt_bind_param($statement, "ssss", $patientID, $date, $message, $department);
+                mysqli_stmt_execute($statement);
+
+                header("Location: ../patient_portal.php?appointment=success");
+                exit();
+            }
+
         }
     }
 ?>
--- patient_portal.php
+++ patient_portal.php
@@ -18,8 +18,8 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"></script>
     
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- jQuery library 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>-->
     <title>Med.io</title>
     </head>
     <body>
