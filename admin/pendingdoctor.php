4fadfe1b75bad30597731f430e9156e824713f0a
Mukit Mahdin <88025578+mahdin70@users.noreply.github.com>
2022-12-24 22:16:47 +0600

    Added "Success" Alert Message ( 3 Sec ) which will show up after every addition

--- admin/pendingdoctor.php
+++ admin/pendingdoctor.php
@@ -4,8 +4,9 @@ error_reporting(0);
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Manage Pending Doctor Request</title>
+
+<head>
+    <title>Manage Pending Doctor Request</title>
 
     <link rel="stylesheet" type="text/css" href="../assets/styles/admin_font.css">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -18,9 +19,10 @@ error_reporting(0);
 
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    </head>
-    <body>
-      
+</head>
+
+<body>
+
     <body style="background-color:white;">
         <?php
         include("../SQL/dbConnect.php")
@@ -31,41 +33,39 @@ error_reporting(0);
             <div class="mr-auto"></div>
 
             <ul class="navbar-nav">
-                <?php 
-                  if(isset($_SESSION['adminUser'])){
+                <?php
+                if (isset($_SESSION['adminUser'])) {
                     $user = $_SESSION['adminUser'];
                     echo '
-                    <li class="nav-item"><a href="#" class="nav-link text-white">'.$user.'</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link text-white">' . $user . '</a></li>
                     <li class="nav-item"><a href="/Med.io/admin_login.php" class="nav-link text-white">Logout</a> </li>
                     ';
-                  }
-                ?>  
+                }
+                ?>
             </ul>
-            
+
         </nav>
 
         <div class="container-fluid">
             <div class="col-md-12">
                 <div class="row">
-                    <div class="col-md-2" style="margin-left:-30px";>
+                    <div class="col-md-2" style="margin-left:-30px" ;>
 
-            <!--Side Navigation-->
-                         <?php
-                           include("./sidenav.php");
-                           include("../SQL/dbConnect.php")
+                        <!--Side Navigation-->
+                        <?php
+                        include("./sidenav.php");
+                        include("../SQL/dbConnect.php")
 
-                         ?>
+                        ?>
                     </div>
-            
-                                <div class="col-md-10">
-                                    <h5 class="text-center" style="font-family:Poppins;margin-top:25px;">Pending Doctor Requests</h5>
-
-                                    <?php
-                                    $query = "SELECT * FROM doctor WHERE Approved = 0";
-                                    $res = mysqli_query($conn, $query);
-
-                                    // Initialize the table output
-                                    $output = "
+                    <div class="col-md-10">
+                        <h5 class="text-center" style="font-family:Poppins;margin-top:25px;">Pending Doctor Requests</h5>
+                        <!-- Alert message -->
+                        <div id="add-alert" class="alert alert-success" style="display:none;">The Doctor is added</div>
+                        <?php
+                        $query = "SELECT * FROM doctor WHERE Approved = 0";
+                        $res = mysqli_query($conn, $query);
+                        $output = "
                                        <table class='table table-striped table-dark table-bordered'>
                                          <tr>
                                           <th>ID</th>
@@ -78,19 +78,13 @@ error_reporting(0);
                                           <th>Action</th>
                                          </tr>
                                         ";
+                        if (mysqli_num_rows($res) < 1) {
 
-                                    // Check if the query returned any rows
-                                    if (mysqli_num_rows($res) < 1) {
-                                        // If there are no rows, display a message
-                                        $output = "<h5 class='text-center' style='font-family:Poppins;'>No New Request</h5>";
-                                    }
-
-                                    // Fetch the rows from the result set as an associative array
-                                    while ($row = mysqli_fetch_assoc($res)) {
-                                        // Store the ID of the doctor in a variable
-                                        $user = $row['ID'];
-                                        // Add a row to the table with the data from the database and the Add Doctor button
-                                        $output .= "
+                            $output = "<h5 class='text-center' style='font-family:Poppins;'>No New Request</h5>";
+                        }
+                        while ($row = mysqli_fetch_assoc($res)) {
+                            $user = $row['ID'];
+                            $output .= "
                                            <tr>
                                              <td>" . $user . "</td>
                                              <td>" . $row['Name'] . "</td>
@@ -104,25 +98,27 @@ error_reporting(0);
                                              </td>
                                            </tr>
                                          ";
+                        }
+                        $output .= "</table>";
+                        echo $output;
+                        if (isset($_GET['user'])) {
+                            $user = $_GET['user'];
+                            $query = "UPDATE doctor SET Approved = 1 WHERE ID = '$user'";
+                            mysqli_query($conn, $query);
+                            echo "<script>
+                                     var searchParams = new URLSearchParams(location.search);
+                                     if (searchParams.has('user')) {
+                                        document.getElementById('add-alert').style.display = 'block';
+                                        setTimeout(function() {
+                                          document.getElementById('add-alert').style.display = 'none';
+                                        }, 3000);
                                     }
-                                    // Close the table
-                                    $output .= "</table>";
-
-                                    // Print the table
-                                    echo $output;
-                                    if (isset($_GET['user'])) {
-                                        // If the parameter is set, store the value in a variable
-                                        $user = $_GET['user'];
-
-                                        // Update the 'Approved' column to 1 for the row with the specified ID
-                                        $query = "UPDATE doctor SET Approved = 1 WHERE ID = '$user'";
-                                        mysqli_query($conn, $query);
-                                    }
-                                    ?>
-
-                                </div>
-                        </div>
+                                  </script>";
+                            }
+                        ?>
                     </div>
                 </div>
+            </div>
+        </div>
     </body>
 </html>
